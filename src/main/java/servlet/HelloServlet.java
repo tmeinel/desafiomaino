30a2e8afb7e56bcdedaac1f5650a858a4003d9ec
tmeinel <tmeinel@DESKTOP-DISN018>
2016-07-24 15:08:31 -0300

    primeiro commit

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(
         name = "MyServlet",
-        urlPatterns = {"/"}
+        urlPatterns = {"/hello"}
     )
 public class HelloServlet extends HttpServlet{
     
@@ -27,7 +27,7 @@ public class HelloServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
+        out.write("hello heroku in Servlet".getBytes());
         out.flush();
         out.close();
     }
--- src/main/webapp/hello.jsp
+++ src/main/webapp/hello.jsp
@@ -1,6 +1,6 @@
 <html>
     <body>
-        <h2>Hello Heroku!</h2>
+        <h2>Hello Heroku in JSP!</h2>
     </body>
 </html>
 Run 
