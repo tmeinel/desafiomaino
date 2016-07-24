1a44ccc9bcd5852b1eae1e6d246878004181ef9b
tmeinel <tmeinel@DESKTOP-DISN018>
2016-07-24 15:16:24 -0300

    primeiro commit

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -7,7 +7,6 @@ package servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,9 +25,6 @@ public class HelloServlet extends HttpServlet{
      @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku in Servlet".getBytes());
-        out.flush();
-        out.close();
+        req.getRequestDispatcher("hello.jsp").forward(req, resp);
     }
 }
